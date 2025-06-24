a01f9ef8d043293c7f5220e7e602d319ba5d6066
QuintessenceCoding <vashishtishika15@gmail.com>
2025-06-24 16:43:48 +0530

    Remove info.py

--- info.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from collections import Counter
-
-X = np.load("features.npy")
-y = np.load("labels.npy")
-
-print("✅ Feature shape:", X.shape)
-print("🧠 Sample label distribution:")
-print(Counter(y))
